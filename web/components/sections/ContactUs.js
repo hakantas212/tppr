7a1204df6014f3600f53b04071e6931da750fd0a
Tayfun Yaltur <tayfunyaltur@gmail.com>
2022-03-05 15:12:09 +0300

    Update ContactUs.js
    values binding on state. and logged into console.

--- web/components/sections/ContactUs.js
+++ web/components/sections/ContactUs.js
@@ -1,12 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
-import styled from "styled-components";
-import imageUrlBuilder from "@sanity/image-url";
-import {Heading, Text} from "../typography";
-import Section from "../Section";
-import client from "../../client";
-import {IntersectionObserver} from "../IntersectionObserver";
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import {Button, Col, Container, Form, Row} from 'react-bootstrap'
+import styled from 'styled-components'
+import imageUrlBuilder from '@sanity/image-url'
+import {Heading, Text} from '../typography'
+import Section from '../Section'
+import client from '../../client'
+import {IntersectionObserver} from '../IntersectionObserver'
 
 function urlFor(source) {
   return imageUrlBuilder(client).image(source)
@@ -35,51 +35,104 @@ const Link = styled.a`
 `
 
 function ContactUs({title, email, phone, media}) {
+  const [_email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    console.log({
+      email: _email,
+      name: name,
+      message: message,
+    })
+  }
+
   return (
     <Section id="contactus">
       <IntersectionObserver id="contactus">
         <Container>
           <Row className="align-items-center">
             <Col xs={12}>
-              <Heading size="sm" bold style={{marginBottom: 64}}>{title}</Heading>
+              <Heading size="sm" bold style={{marginBottom: 64}}>
+                {title}
+              </Heading>
             </Col>
-            <Col lg={{order:1,span:4}} xs={{order:3,span:12}} className="mb-5 mb-lg-0">
-              <img className="w-100" src="contact_us.png" alt="contact_us"/>
+            <Col lg={{order: 1, span: 4}} xs={{order: 3, span: 12}} className="mb-5 mb-lg-0">
+              <img className="w-100" src="contact_us.png" alt="contact_us" />
             </Col>
-            <Col lg={{order:2,span:4}} xs={{order:4,span:12}} className="text-center mb-5 mb-lg-0">
-              <Img src="hero_logo.svg" alt="hero_logo"/>
+            <Col
+              lg={{order: 2, span: 4}}
+              xs={{order: 4, span: 12}}
+              className="text-center mb-5 mb-lg-0"
+            >
+              <Img src="hero_logo.svg" alt="hero_logo" />
               <Link href={`mailto:${email}`}>
-                <Text className="mb-4" color="#A0A3BD">{email}</Text>
+                <Text className="mb-4" color="#A0A3BD">
+                  {email}
+                </Text>
               </Link>
               <Link href={`tel:${phone}`}>
-                <Text className="mb-4" color="#A0A3BD">{phone}</Text>
+                <Text className="mb-4" color="#A0A3BD">
+                  {phone}
+                </Text>
               </Link>
               <SocialMediaList>
-                {media ?  media.map((entity) => (
-                  <li key={entity._key}>
-                    <a href={entity.link} target="_blank" rel="noreferrer">
-                      <img src={urlFor(entity.image).auto('format').fit('max').toString()} alt={entity.title} />
-                    </a>
-                  </li>
-                )): ""}
+                {media
+                  ? media.map((entity) => (
+                      <li key={entity._key}>
+                        <a href={entity.link} target="_blank" rel="noreferrer">
+                          <img
+                            src={urlFor(entity.image).auto('format').fit('max').toString()}
+                            alt={entity.title}
+                          />
+                        </a>
+                      </li>
+                    ))
+                  : ''}
               </SocialMediaList>
             </Col>
-            <Col lg={4} xs={{order:2,span:12}}>
-              <Form>
+            <Col lg={4} xs={{order: 2, span: 12}}>
+              <Form onSubmit={sendEmail}>
                 <Form.Group className="mb-4" controlId="formBasicName">
-                  <Form.Control type="text" size="lg" placeholder="Name" />
+                  <Form.Control
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    size="lg"
+                    placeholder="Name"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-4" controlId="formBasicEmail">
-                  <Form.Control type="email" size="lg" placeholder="E-Mail" />
+                  <Form.Control
+                    value={_email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    size="lg"
+                    placeholder="E-Mail"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-4" controlId="formBasicMessage">
-                  <Form.Control as="textarea" size="lg" rows={3} placeholder="Message" />
+                  <Form.Control
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    as="textarea"
+                    size="lg"
+                    rows={3}
+                    placeholder="Message"
+                  />
                 </Form.Group>
 
                 <div className="d-grid gap-2">
-                  <Button variant="primary" type="submit" size="lg" className="text-white" style={{marginBottom:'1rem'}}>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    size="lg"
+                    className="text-white"
+                    style={{marginBottom: '1rem'}}
+                  >
                     Submit
                   </Button>
                 </div>
