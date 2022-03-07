442c61ce8117d2c62ced1a054d394fe0e05454f1
Hakan Tas <hakantas212@gmail.com>
2022-03-07 13:27:13 +0100

    fav pack

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -21,6 +21,12 @@ export default class MyDocument extends Document {
       <Html lang={this.props.lang || 'en'}>
         <Head>
           <link rel="shortcut icon" href="../public/favicon.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png"/>
+          <link rel="manifest" href="../public/site.webmanifest"/>
+          <link rel="mask-icon" href="../public/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
           <script
                   async
                   src="https://www.googletagmanager.com/gtag/js?id=[G-TK36GB1T83]"
